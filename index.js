72ea75f26dc7d92fc79dea86413f67bbf94f3b97
Abdisemed Abdullahi <abdisemed20@gmail.com>
2023-09-22 20:28:34 +0300

    completed assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution in this file!
+const employee = {
+    name: "Mankulo",
+    streetAdress: "12th street Eastleigh"
+}
+
+
+
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    const updatedEmployee= {
+        ...employee,
+        [key]: value
+    };
+    console.log(updatedEmployee);
+    return updatedEmployee;
+}
+ updateEmployeeWithKeyAndValue(employee, "name", "Sam");
+
+ function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAdress", "12 Broadway");
+
+function deleteFromEmployeeByKey (employee, key) {
+    
+    delete employee[key];   
+    console.log(employee);
+    return employee;
+}
+deleteFromEmployeeByKey(employee, "name")
+
+
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key];
+    return employee;
+}
+destructivelyDeleteFromEmployeeByKey(employee, "streetAdress");
+
+function nonDestructivelyDeleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee }; // Create a new object using the spread operator
+    delete newEmployee[key]; // Delete the specified key from the new object
+    return newEmployee;
+}
+const updatedEmployee = nonDestructivelyDeleteFromEmployeeByKey(employee, "name");
+console.log(updatedEmployee);
+
